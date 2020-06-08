f7377cb1a437f8e809208f3cebd73eff50c948cb
George Kosmidis <georgekosmidis@outlook.com>
2020-06-08 09:32:39 +0200

    Solution Structure

---
+++ src/RegistrationService.Core/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RegistrationService.Core
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/RegistrationService.Infrastructure/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RegistrationService.Infrastructure
+{
+    public class Class1
+    {
+    }
+}
